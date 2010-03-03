208e28acd89a76c508c2aa34b005bce1f6ba6724
Matt Wildig <matt@mattwildig.co.uk>
2010-03-03 00:31:05 +0000

    Fix for trailing slash problem in passenger

--- server.rb
+++ server.rb
@@ -3,6 +3,10 @@ require 'sinatra'
 require 'haml'
 require 'lib/markov'
 
+before do
+  request.env['PATH_INFO'] = '/' if request.env['PATH_INFO'].empty?
+end
+
 get '/' do
   @txt = Markov::Chain.new(File.new('data/alice.txt'), :output_words => 600).generate
     
